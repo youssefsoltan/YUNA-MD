5613f03225569da3a21ddb5abfd8d368eee9dfe7
MAYO1 <82735720+ahmed1p7@users.noreply.github.com>
2022-12-29 19:57:47 +0200

    ༺════ •⊰『 𝒚𝒖𝒏𝒂 𝒃𝒐𝒕 』⊱• ════༻

--- plugins/rpg-rob.js
+++ plugins/rpg-rob.js
@@ -1,23 +1,58 @@
 let ro = 3000
 let handler = async (m, { conn, usedPrefix, command}) => {
 let time = global.db.data.users[m.sender].lastrob + 7200000
-if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*� !   ${msToTime(time - new Date())}   *`
+if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*عد بعد ${msToTime(time - new Date())} لكي تسرق*`
 let who
 if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
 else who = m.chat
-if (!who) throw `*[]      .*`
-if (!(who in global.db.data.users)) throw `*[]          .*`
+if (!who) throw `*[❗] المستخدم ليس في قاعدة البيانات الخاصة بي*`
+if (!(who in global.db.data.users)) throw `*[❗] وضع علامة على شخص ما لسرقته. .*`
 let users = global.db.data.users[who]
 let rob = Math.floor(Math.random() * ro)
-if (users.exp < rob) return m.reply(` @${who.split`@`[0]} tiene menos de *${ro} xp*\n    v":`, null, { mentions: [who] })    
+if (users.exp < rob) return m.reply(`😔 @${who.split`@`[0]} لديه أقل من *${ro} xp*\nلا تسرق من رجل فقير v":`, null, { mentions: [who] })    
 global.db.data.users[m.sender].exp += rob
 global.db.data.users[who].exp -= rob 
-m.reply(`*  ${rob} XP a @${who.split`@`[0]}*`, null, { mentions: [who] })
+m.reply(`*لقد تمت السرقه ${rob} XP من @${who.split`@`[0]}*`, null, { mentions: [who] })
 global.db.data.users[m.sender].lastrob = new Date * 1
 }
-handler.help = ['']
+handler.help = ['سرقه']
 handler.tags = ['econ']
-handler.command = ['', '']
+handler.command = ['سرقه', 'rob']
+export default handler  
+function msToTime(duration) {
+var milliseconds = parseInt((duration % 1000) / 100),
+seconds = Math.floor((duration / 1000) % 60),
+minutes = Math.floor((duration / (1000 * 60)) % 60),
+hours = Math.floor((duration / (1000 * 60 * 60)) % 24)
+hours = (hours < 10) ? "0" + hours : hours
+minutes = (minutes < 10) ? "0" + minutes : minutes
+seconds = (seconds < 10) ? "0" + seconds : seconds
+return hours + "ساعة " + minutes + "دقيق"}
+/*
+let ro = 3000
+let handler = async (m, { conn, usedPrefix, command}) => {
+let time = global.db.data.users[m.sender].lastrob + 7200000
+if (new Date - global.db.data.users[m.sender].lastrob < 7200000) throw `*⏱️¡Hey! Espera ${msToTime(time - new Date())} para volver a robar*`
+let who
+if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false
+else who = m.chat
+if (!who) throw `*[❗] ETIQUETA A ALGUIEN PARA ROBAR.*`
+if (!(who in global.db.data.users)) throw `*[❗] EL USUARIO NO SE ENCUENTRA EN MI BASE DE DATOS.*`
+let users = global.db.data.users[who]
+let rob = Math.floor(Math.random() * ro)
+if (users.exp < rob) return m.reply(`😔 @${who.split`@`[0]} tiene menos de *${ro} xp*\nNo robes a un pobre v":`, null, { mentions: [who] })    
+global.db.data.users[m.sender].exp += rob
+global.db.data.users[who].exp -= rob 
+global.db.data.users[m.sender].limit += rob
+global.db.data.users[who].limit -= rob 
+global.db.data.users[m.sender].money += rob
+global.db.data.users[who].money -= rob 
+m.reply(`*‣ Robaste ${rob} ${'Xp', 'Gatacoins 🐈', 'Diamante💎'} a @${who.split`@`[0]}*`, null, { mentions: [who] })
+global.db.data.users[m.sender].lastrob = new Date * 1
+}
+handler.help = ['rob']
+handler.tags = ['econ']
+handler.command = ['robar', 'rob']
 export default handler  
 function msToTime(duration) {
 var milliseconds = parseInt((duration % 1000) / 100),
@@ -28,3 +63,4 @@ hours = (hours < 10) ? "0" + hours : hours
 minutes = (minutes < 10) ? "0" + minutes : minutes
 seconds = (seconds < 10) ? "0" + seconds : seconds
 return hours + " Hora(s) " + minutes + " Minuto(s)"}
+*/
